1c1a29657eb91a11669ee0d70e1385cfdd82bc91
Lee Torres <grafrafraftorres28@gmail.com>
2025-12-18 11:56:30 +0800

    Update api.js

--- mobile/src/config/api.js
+++ mobile/src/config/api.js
@@ -2,7 +2,7 @@
 // Replace with your computer's IP address when testing on physical devices
 // Use 'localhost' only when testing on emulator/simulator on the same machine
 
-export const API_BASE_URL = 'http://192.168.8.42:5000';
+export const API_BASE_URL = 'http://192.168.0.36:5000';
 
 export const API_ENDPOINTS = {
   // Auth
